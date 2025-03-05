e59b7c800d5d116da089401c905f4d49a892b238
Nitesh <kumarnitesh2601@gmail.com>
2025-03-05 14:54:44 +0530

    refresh issue solved

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 
+// export default defineConfig({
+//   plugins: [react()]
+  
+  
+// })
+
+
+
+
 export default defineConfig({
-  plugins: [react()]
-  
-  
-})
+  plugins: [react()],
+  base: '/',  // Ensures correct asset loading in Vercel
+  build: {
+    outDir: 'dist',  // Ensures correct build output
+  }
+});
